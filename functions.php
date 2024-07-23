5999353c4765a36c92d36544fc83f55b4d593ca8
Kanimet <kanimet200127@gmail.com>
2024-07-23 17:10:38 +0600

    Created component: sidebar

--- functions.php
+++ functions.php
@@ -4,9 +4,11 @@ add_action('wp_enqueue_scripts', 'sparrow_enqueue_scripts');
 
 add_action('wp_footer', 'sparrow_wp_footer');
 
-add_action('after_setup_theme', 'sparrow_setup');
+add_action('after_setup_theme', 'sparrow_after_setup_theme');
 
-function sparrow_setup(): void
+add_action('widgets_init', 'sparrow_widgets_init');
+
+function sparrow_after_setup_theme(): void
 {
     register_nav_menu('top', 'Top Menu');
     register_nav_menu('footer', 'Footer Menu');
@@ -34,4 +36,17 @@ function sparrow_wp_footer(): void
     wp_enqueue_script('jquery.flexslider', $path . 'js/jquery.flexslider.js', ['jquery']);
     wp_enqueue_script('doubletaptogo', $path . 'js/doubletaptogo.js', ['jquery']);
     wp_enqueue_script('init', $path . 'js/init.js', ['jquery']);
+}
+
+function sparrow_widgets_init(): void
+{
+    register_sidebar([
+        'name' => 'Left Sidebar',
+        'id' => 'left_sidebar',
+        'description' => 'Left Sidebar Description',
+        'before_widget' => '<div class="widget %2$s">',
+        'after_widget' => "</div>\n",
+        'before_title' => '<h5 class="widgettitle">',
+        'after_title' => "</h5>\n",
+    ]);
 }
