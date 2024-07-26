e7067f427f8c8147bea77d3677fae3b6e39f6ac9
Kanimet <kanimet200127@gmail.com>
2024-07-26 14:18:42 +0600

    Added the posts to the home page

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php get_header(); ?>
 
+<section id="journal">
+    <div class="row">
+        <div class="twelve columns align-center">
+            <h1>Our latest posts and rants.</h1>
+        </div>
+    </div>
+
+    <div class="blog-entries">
+        <?php get_template_part('components/home', 'posts') ?>
+    </div>
+</section>
+
 <?php get_footer(); ?>
