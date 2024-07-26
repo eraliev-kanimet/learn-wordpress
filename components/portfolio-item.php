3609d60a5e81e3c7d45a27c826fcca5ee808fd15
Kanimet <kanimet200127@gmail.com>
2024-07-27 00:08:28 +0600

    Created components: portfolio-list and portfolio-item

---
+++ components/portfolio-item.php
@@ -0,0 +1,17 @@
+<?php
+$link = get_permalink();
+?>
+
+<div class='columns portfolio-item'>
+    <div class='item-wrap'>
+        <a href='<?= $link ?>'>
+            <?php the_post_thumbnail() ?>
+            <div class='overlay'></div>
+            <div class='link-icon'><i class='fa fa-link'></i></div>
+        </a>
+        <div class='portfolio-item-meta'>
+            <h5><a href='<?= $link ?>'><?php the_title() ?></a></h5>
+            <p><?php the_excerpt() ?></p>
+        </div>
+    </div>
+</div>
