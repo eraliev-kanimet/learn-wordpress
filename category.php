e49d796798d1bce6f1150ee2f1b1fc251bd8bbda
Kanimet <kanimet200127@gmail.com>
2024-07-26 23:30:27 +0600

    Added a “hero” and “tweets” component to the category page

--- category.php
+++ category.php
@@ -1,6 +1,16 @@
-<?php get_header(); ?>
+<?php
+
+get_header();
+
+get_template_part('components/hero', null, [
+    'header' => 'Category<span>.</span>',
+    'text' => 'Aenean condimentum, lacus sit amet luctus lobortis, dolores et quas molestias excepturi enim tellus ultrices elit, amet consequat enim elit noneas sit amet luctu.',
+])
+
+?>
 
 <div class="content-outer">
+
     <div id="page-content" class="row">
 
         <div id="primary" class="eight columns">
@@ -12,6 +22,13 @@
         </div>
 
     </div>
+
 </div>
 
-<?php get_footer(); ?>
+<?php
+
+get_template_part('components/tweets');
+
+get_footer();
+
+?>
