bbd648867e20a870ee98eab0aef1bd029bf4f7cb
Kanimet <kanimet200127@gmail.com>
2024-07-27 16:42:15 +0600

    Made a page.php file for the pages

---
+++ page.php
@@ -0,0 +1,17 @@
+<?php
+
+get_header();
+
+?>
+
+<div class="content-outer">
+    <div id="page-content" class="row page">
+        <?php the_content(); ?>
+    </div>
+</div>
+
+<?php
+
+get_footer();
+
+?>
